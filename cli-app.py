68c633dfd8f4ccaff9d2b47ef3a4d0cf7560976f
Rakesh Kumar T <rakeshptajlapur@gmail.com>
2024-04-29 10:03:49 +0530

    Initial todolist app with add/edit/delete/show operations.

---
+++ cli-app.py
@@ -0,0 +1,38 @@
+print("Welcome to the Todo Application")
+todolist = []
+
+while True:
+    user_prompt = input("Type the actions you would like to perform: add/edit/delete/show/save/exit !")
+    match user_prompt:
+        case 'add':
+           newtodoitem = input("Enter the new todo item : ")
+           todolist.append(newtodoitem)
+           print(todolist)
+
+
+        case 'edit':
+            print(todolist)
+            todoitem2edit = input("Which item you want to edit :")
+            # identify the index of the item to be edited
+            todoitem2edit_index = todolist.index(todoitem2edit)
+            itemafter_edit = input("Enter the todo item after edit :")
+            todolist[todoitem2edit_index] = itemafter_edit
+
+
+        case 'delete':
+            print(todolist)
+            todoitem2delete = input("Which todo item you want to delete : ")
+            # identify the index of the item to be deleted
+            todoitem2delete_index = todolist.index(todoitem2delete)
+            print(todolist[todoitem2delete_index], " has been deleted successfully!")
+            del todolist[todoitem2delete_index]
+
+
+        case 'show':
+            print("current todolist are as follows:")
+            print(todolist)
+
+
+
+        case 'exit':
+            break
